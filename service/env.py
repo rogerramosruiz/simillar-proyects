c29290b752235b0a85fc7a13f38b546f34ae3ab4
rogerramosruiz <rogerramosruizdev@gmail.com>
2023-10-08 10:50:43 -0400

    docker

--- service/env.py
+++ service/env.py
@@ -1,7 +1,9 @@
-PUBLIC_DIR='upload/files'
-FILENAME_SIZE=10
-DB_HOST=""
-DB_NAME=""
-DB_USER=""
-DB_PASSWORD=0
-DB_PORT=0
+import os 
+
+PUBLIC_DIR    = os.environ.get('PUBLIC_DIR')
+FILENAME_SIZE = int(os.environ.get('FILENAME_SIZE')) 
+DB_HOST       = os.environ.get('DB_HOST')
+DB_NAME       = os.environ.get('DB_NAME')
+DB_USER       = os.environ.get('DB_USER')
+DB_PASSWORD   = int(os.environ.get('DB_PASSWORD'))
+DB_PORT       = int(os.environ.get('DB_PORT'))
